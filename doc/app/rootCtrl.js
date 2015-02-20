c5c7cd903bbe62a8e2b819075ea9dff79a8148f6
eisneim <eisneim1@gmail.com>
2015-02-20 14:05:02 +0800

    [done] change page title;

--- doc/app/rootCtrl.js
+++ doc/app/rootCtrl.js
@@ -1,6 +1,12 @@
 app.controller('rootCtrl',['$scope',function($scope){
 	var rootCtrl = this;
 
+	$scope.$root.$on('$routeChangeSuccess',function(e,current){
+		// change page title
+		if( current.$$route && current.$$route.title) 
+			document.title = current.$$route.title + ' - Angular-UI-Awesome';
+	})
+
 }]);
 
 app.controller('navCtrl',['$scope',function($scope){
