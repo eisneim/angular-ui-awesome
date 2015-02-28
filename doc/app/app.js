30c279ad162a6e48cc124233ed33ab7b52740140
eisneim <eisneim1@gmail.com>
2015-02-28 16:55:13 +0800

    [change] fallback to nonHTML5 route mode
    to make sure github pages will not need a server

--- doc/app/app.js
+++ doc/app/app.js
@@ -7,7 +7,7 @@ app.run([function(){
 }]);
 
 app.config(function($routeProvider,$locationProvider){
-	$locationProvider.html5Mode(true);
+	// $locationProvider.html5Mode(true);
 
 	$routeProvider
 	.when('/',{
