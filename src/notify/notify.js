4a73d3bc980017b0a5962c73f741ffb3ffa94983
eisneim <eisneim1@gmail.com>
2015-04-17 21:34:33 +0800

    [start] $ua-notify for user notification;

---
+++ src/notify/notify.js
@@ -0,0 +1,111 @@
+ua.factory('$ua-notify',function(){
+	var id = 0;
+
+	function Notify( opt ){
+		this.id = opt.id || _idGen();
+		this.container = opt.container || document.body ;
+		this.life = opt.life || 4000,
+		// defaults to true;
+		this.clickToHide = !opt.clickNotHide;
+		this.type = opt.type || 'full';// full, block
+		/**
+		 * if type is block, you have to provide position: top or bottom, left or right;
+		 */
+		if(opt.position && typeof opt.position == 'object' ){
+			this.pos = opt.position;
+		}else{
+			this.pos = {top:'0em',right:'0em'};
+		}
+
+	}
+
+	var pt = Notify.prototype;
+
+	pt._showMsg = function(type,msg,title){
+		this.elm = document.createElement('div');
+
+
+		this.elm.style.position = 'fixed';
+
+		this.pos.top ? ( this.elm.style.top = this.pos.top ) : ( this.elm.style.bottom = this.pos.bottom );
+		
+
+		if(this.type == 'block'){
+			this.pos.left ? ( this.elm.style.left = this.pos.left ) : ( this.elm.style.right = this.pos.right );
+		}
+
+		this.elm.className = 'nb-notify notify-'+type + ' notify-'+this.type;
+
+		var tpl = '';
+		if(title){
+			tpl += '<h4>'+title+'</h4>';
+		}
+		tpl += '<p>'+msg+'</p>';
+
+		this.elm.innerHTML = tpl;
+
+		// append to container
+		this.container.appendChild(this.elm);
+		if(this.clickToHide){
+			this.elm.onclick = this.suicide.bind(this);
+		}
+		// suicide timer
+		setTimeout( this.suicide.bind(this), this.life );
+	}
+
+	pt.suicide = function(){
+		// prevent click multi times
+		if( this.leaving ) return;
+		this.leaving = true;
+
+		this.elm.classList.add('hidding');
+
+		setTimeout(function(){
+			this.elm.remove();
+		}.bind(this),500)
+	}
+
+	pt.error = function( msg, title ){
+		// 超级装逼的写法，我也来脱了裤子放P一会，免得产品经理说我写的代码没有经过精细的设计
+		Array.prototype.unshift.call( arguments ,'error');
+		this._showMsg.apply(this, arguments )
+		/*
+			其实只需要这样：   this._showMsg('error',msg,title );
+		 */
+	}
+
+	pt.success = function(msg, title){
+		Array.prototype.unshift.call(arguments,'success');
+		this._showMsg.apply(this, arguments )
+	}
+
+	pt.info = function(msg, title){
+		Array.prototype.unshift.call(arguments,'info');
+		this._showMsg.apply(this, arguments )
+	}
+
+
+	function _idGen(prefix){
+		return (prefix||'nbNoti-elm-') + id++;
+	}
+
+	module.exports = function(opt){
+		if(opt){
+			return new Notify(opt);
+		}
+
+		return new Notify({
+			container: document.body,
+			life: 5000,
+			type: 'full',
+			position: {
+				top: '0px',
+				left: '0px',
+			}
+		});
+
+	}
+
+
+
+});
