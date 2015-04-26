e40a75a13238d0a09452b2b13150c3993c4b59fe
eisneim <eisneim1@gmail.com>
2015-04-26 22:45:53 +0800

    [add] modal.js basic class

--- src/modal/modal.js
+++ src/modal/modal.js
@@ -2,11 +2,38 @@ ua.factory('$uaModal',[function(){
 
 	class Modal {
 		constructor( opt ) {
-			
+			this.type = opt.type || 'confirm';// prompt
+			// this.event = opt.event; // get starting x,y position 
+				 
 
 		}
 
+		_show(){
+
+		}
 		
+		_destory(  ){
+
+		}
+
+		confirm( message ){
+
+		}
+
+		prompt( message ){
+
+		}
+
+		custom( opt ){
+
+
+		}
+
+
+	}
+	
+	return function(opt){
+		return new Modal( opt );
 	}
 
 }])
